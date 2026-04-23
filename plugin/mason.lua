ab7e714d9cfbe53a35ab30c3320770f04146de53
MHashir09 <mhia09@proton.me>
2026-04-23 08:06:50 +0500

    Feat: Add mason.lua for lsp installation and configuration, along with few linters

---
+++ plugin/mason.lua
@@ -0,0 +1,43 @@
+-- // We will use mason to manage our lsps // --
+vim.pack.add({
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+})
+
+require("mason").setup()
+
+-- // I am enabling and installing the lsps I use // --
+-- // You can add or remove the ones you want or dont want respectively, in similar fashion // --
+-- // You can manage them through ":Mason" // --
+
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"bashls",
+		"clangd",
+		"html",
+		"cssls",
+		"lua_ls",
+		"pyright",
+		"ts_ls",
+		"rust_analyzer",
+        "eslint",  -- linter for js_ts/html/css
+        "ruff", -- linter for python
+   },
+    automatic_installation = true,
+})
+
+vim.lsp.enable({
+	"bashls",
+	"clangd",
+	"html",
+	"cssls",
+	"lua_ls",
+	"pyright",
+	"ts_ls",
+	"rust_analyzer",
+    "eslint",
+    "ruff"
+})
+
+vim.diagnostic.config({ virtual_text = true }) -- controls how lsp diagnostics are displayed
