088e0a6745a572d2d3621e3af63b20c0b0501150
MHashir09 <mhia09@proton.me>
2026-04-26 12:02:17 +0500

    Feat: Replace the comments with description for keymaps to be read my fzf-lua

--- lua/global-keymaps.lua
+++ lua/global-keymaps.lua
@@ -1,47 +1,47 @@
--- // ####  GLOBAL KEYMAPS  ##### // --
 -- // ########################### // --
+-- // ####  GLOBAL KEYMAPS  ##### // --
 -- // ########################### // --
 
 local keymap = vim.keymap.set
-local s = { silent = true } -- suppresses commands from being echoed in the command line, which is visually noisy
 
-vim.g.mapleader = " " -- sets leader key to "space bar"
+vim.g.mapleader = " "
 
-keymap("n", "<space>", "<Nop>", s) -- if space is pressed in normal mode, do nothing. Prevents flickers
-keymap("i", "jk", "<Esc>", s) -- because I like to use "jk" to enter normal mode rather than esc
-keymap("n", "sa", "ggVG", s) -- selects all text in the buffer. gg(jumps to the first line), V(enters visual mode), G(jumps to last line)
-keymap("n", "<C-d>", "<C-d>zz",s) -- Scroll down and center the cursor
-keymap("n", "<C-u>", "<C-u>zz", s) -- Scroll up and center the cursor
-keymap("n", "<C-h>", "<C-w>h", s) -- move to left split
-keymap("n", "<C-l>", "<C-w>l", s) -- move to right split
-keymap("n", "<C-j>", "<C-w>j", s) -- move to split below
-keymap("n", "<C-k>", "<C-w>k", s) -- move to split above
-keymap("v", "<Leader>p", '"_dP', s) -- paste over selected text without losing the afore-copied text
-keymap("n", "<Leader>_", "<cmd>vsplit<CR>", s) -- split the window vertically
-keymap("n", "<Leader>-", "<cmd>split<CR>", s) -- split the window horizontally
-keymap("n", "<leader>up", '<cmd>lua vim.pack.update()<CR>') -- updates vim.pack packages
-
+keymap("n", "<space>", "<Nop>", { silent = true, desc = "Disable space in normal mode" })
+keymap("i", "jk", "<Esc>", { silent = true, desc = "Exit insert mode" })
+keymap("n", "sa", "ggVG", { silent = true, desc = "Select all" })
+keymap("n", "<C-d>", "<C-d>zz", { silent = true, desc = "Scroll down and center" })
+keymap("n", "<C-u>", "<C-u>zz", { silent = true, desc = "Scroll up and center" })
+keymap("n", "<C-h>", "<C-w>h", { silent = true, desc = "Move to left split" })
+keymap("n", "<C-l>", "<C-w>l", { silent = true, desc = "Move to right split" })
+keymap("n", "<C-j>", "<C-w>j", { silent = true, desc = "Move to split below" })
+keymap("n", "<C-k>", "<C-w>k", { silent = true, desc = "Move to split above" })
+keymap("v", "<Leader>p", '"_dP', { silent = true, desc = "Paste without overwriting register" })
+keymap("n", "<Leader>_", "<cmd>vsplit<CR>", { silent = true, desc = "Vertical split" })
+keymap("n", "<Leader>-", "<cmd>split<CR>", { silent = true, desc = "Horizontal split" })
 
 keymap("n", "j", function()
     return tonumber(vim.api.nvim_get_vvar("count")) > 0 and "j" or "gj"
-end, { expr = true, silent = true }) -- j navigates visual rows normally, but uses real lines when given a count (e.g. 5j)
+end, { expr = true, silent = true, desc = "Move down (visual lines)" })
+
 keymap("n", "k", function()
     return tonumber(vim.api.nvim_get_vvar("count")) > 0 and "k" or "gk"
-end, { expr = true, silent = true }) -- k navigates visual rows normally, but uses real lines when given a count (e.g. 8k)
+end, { expr = true, silent = true, desc = "Move up (visual lines)" })
+
 keymap("n", "<Leader>f", function()
     vim.lsp.buf.format({ async = true })
-end, s) -- formats the current buffer using LSP, async = true so formatting doesn't block the editor on large files
+end, { silent = true, desc = "Format buffer" })
+
 keymap("n", "<leader>b", function()
     vim.ui.open(vim.fn.expand("%:p"))
 end, { silent = true, desc = "Open file in browser" })
 
+keymap("t", "<space>", "<space>", { silent = true, desc = "Space in terminal mode" })
+
 local functions = require("functions-autocmds")
-keymap("n", "<Leader>t", functions.toggle_terminal, s) -- toggle terminal
-keymap("t", "jk", "<C-\\><C-N>", s)           -- exit terminal mode
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", s)  -- move to left split
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", s)  -- move to split below
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", s)  -- move to split above
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", s)  -- move to right split
-keymap("t", "<Leader>_", "<cmd>vsplit | terminal<CR>", s) -- Open new terminal in vertical split
-
-
+keymap("n", "<Leader>t", functions.toggle_terminal, { silent = true, desc = "Toggle terminal" })
+keymap("t", "jk", "<C-\\><C-N>", { silent = true, desc = "Exit terminal mode" })
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", { silent = true, desc = "Terminal: move to left split" })
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", { silent = true, desc = "Terminal: move to split below" })
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", { silent = true, desc = "Terminal: move to split above" })
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", { silent = true, desc = "Terminal: move to right split" })
+keymap("t", "<Leader>_", "<cmd>vsplit | terminal<CR>", { silent = true, desc = "New terminal in vertical split" })
