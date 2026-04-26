fc15a976d1a2b9397e66d695a55d0b50ed4240bd
MHashir09 <mhia09@proton.me>
2026-04-26 12:10:15 +0500

    Feat: Replace comments with descriptions of keymaps for fzf-lua keymap search

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,32 +1,37 @@
 -- // Using bufferline for visual buffer tabs // --
 
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "UIEnter", -- load after UI is ready
-    config = function()
-        require("bufferline").setup({
-            options = {
-                diagnostics = "nvim_lsp",
-                separator_style = "thick",
-                tab_size = 26,
-                max_name_length = 30,
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-                always_show_bufferline = true,
-            },
-        })
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "UIEnter",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				diagnostics = "nvim_lsp",
+				separator_style = "thick",
+				tab_size = 26,
+				max_name_length = 30,
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+				always_show_bufferline = true,
+			},
+		})
 
-        -- // switch buffers with Alt+number
-        for i = 1, 9 do
-            vim.keymap.set("n", "<A-" .. i .. ">", "<Cmd>BufferLineGoToBuffer " .. i .. "<CR>", { silent = true })
-        end
-        vim.keymap.set("n", "<A-0>", "<Cmd>BufferLineGoToBuffer -1<CR>", { silent = true }) -- last buffer
+		-- // switch buffers with Alt+number
+		for i = 1, 9 do
+			vim.keymap.set(
+				"n",
+				"<A-" .. i .. ">",
+				"<Cmd>BufferLineGoToBuffer " .. i .. "<CR>",
+				{ silent = true, desc = "Go to buffer " .. i }
+			)
+		end
+		vim.keymap.set("n", "<A-0>", "<Cmd>BufferLineGoToBuffer -1<CR>", { silent = true, desc = "Go to last buffer" })
 
-        -- // buffer management
-        vim.keymap.set("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { silent = true })        -- pins buffer in place
-        vim.keymap.set("n", "<leader>bd", "<Cmd>bdelete<CR>", { silent = true })                    -- close current buffer
-        vim.keymap.set("n", "<leader>br", "<Cmd>BufferLineCloseRight<CR>", { silent = true })       -- close all buffers to the right
-        vim.keymap.set("n", "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", { silent = true })        -- close all buffers to the left
-    end,
+		-- // buffer management
+		vim.keymap.set("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { silent = true, desc = "Pin buffer" })
+		vim.keymap.set("n", "<leader>bd", "<Cmd>bdelete<CR>", { silent = true, desc = "Close buffer" })
+		vim.keymap.set("n", "<leader>br", "<Cmd>BufferLineCloseRight<CR>", { silent = true, desc = "Close buffers to the right" })
+		vim.keymap.set("n", "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", { silent = true, desc = "Close buffers to the left" })
+	end,
 }
