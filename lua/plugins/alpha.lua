18a40a25474379a163ee96c57f315d641ffc21a4
MHashir09 <mhia09@proton.me>
2026-04-30 04:59:40 +0500

    Fix: Make the p button list directories inside project directories instead of their files

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -29,10 +29,34 @@ return {
         -- // dashboard buttons
         dashboard.section.buttons.val = {
             dashboard.button("f", " Find file", "<cmd>FzfLua files<CR>"),
-            dashboard.button("n", " New file",  ":ene <BAR> startinsert <CR>"),
-            dashboard.button("p", " Projects",  "<cmd>FzfLua files cwd=~/Projects<CR>"),
-            dashboard.button("r", " Recent",    "<cmd>FzfLua oldfiles<CR>"),
-            dashboard.button("q", " Quit",      ":qa<CR>"),
+            dashboard.button("n", " New file", ":ene <BAR> startinsert <CR>"),
+            -- Project requires you to have a ~/Projects directory
+            dashboard.button("p", " Projects", function()
+                local projects_dir = vim.fn.expand("~/Projects")
+                local dirs = vim.fn.globpath(projects_dir, "*", false, true)
+                -- filter to directories only
+                dirs = vim.tbl_filter(function(d)
+                    return vim.fn.isdirectory(d) == 1
+                end, dirs)
+                -- strip full path to show only folder names
+                local names = vim.tbl_map(function(d)
+                    return vim.fn.fnamemodify(d, ":t")
+                end, dirs)
+                require("fzf-lua").fzf_exec(names, {
+                    prompt = "Projects❯ ",
+                    actions = {
+                        ["default"] = function(selected)
+                            if selected[1] then
+                                local project = projects_dir .. "/" .. selected[1]
+                                vim.cmd("cd " .. project)
+                                require("fzf-lua").files({ cwd = project })
+                            end
+                        end,
+                    },
+                })
+            end),
+            dashboard.button("r", " Recent", "<cmd>FzfLua oldfiles<CR>"),
+            dashboard.button("q", " Quit", ":qa<CR>"),
         }
 
         -- // hide ui elements when dashboard is open
@@ -41,18 +65,18 @@ return {
             nested = true,
             callback = function()
                 vim.o.showtabline = 0
-                vim.o.laststatus  = 0
+                vim.o.laststatus = 0
                 pcall(function()
                     require("bufferline.state").set_offset(0)
                 end)
                 vim.schedule(function()
-                    vim.opt_local.number         = false
+                    vim.opt_local.number = false
                     vim.opt_local.relativenumber = false
-                    vim.opt_local.signcolumn     = "no"
-                    vim.opt_local.foldenable     = false
-                    vim.opt_local.statuscolumn   = ""
-                    vim.o.showtabline            = 0
-                    vim.o.laststatus             = 0
+                    vim.opt_local.signcolumn = "no"
+                    vim.opt_local.foldenable = false
+                    vim.opt_local.statuscolumn = ""
+                    vim.o.showtabline = 0
+                    vim.o.laststatus = 0
                 end)
             end,
         })
@@ -73,13 +97,13 @@ return {
         vim.api.nvim_create_autocmd("BufUnload", {
             pattern = "<buffer>",
             callback = function()
-                vim.o.showtabline  = 2                          -- restore tabline
-                vim.o.laststatus   = 2                          -- restore statusline
-                vim.o.statuscolumn = "%#LineNr#  %{v:lnum}  "  -- restore statuscolumn
+                vim.o.showtabline = 2                         -- restore tabline
+                vim.o.laststatus = 2                          -- restore statusline
+                vim.o.statuscolumn = "%#LineNr#  %{v:lnum}  " -- restore statuscolumn
                 local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
                 hl.blend = 0
                 vim.api.nvim_set_hl(0, "Cursor", hl)
-                vim.opt.guicursor:remove("a:Cursor/lCursor")   -- restore cursor
+                vim.opt.guicursor:remove("a:Cursor/lCursor") -- restore cursor
             end,
         })
 
